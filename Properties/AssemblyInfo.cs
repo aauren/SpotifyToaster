357d564d1964f92766b80864e525c9c39808fa2c
Aaron U'Ren <aauren@gmail.com>
2014-08-22 15:36:03 -0700

    Added assembly information

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpotifyToaster")]
+[assembly: AssemblyDescription("Presents toast notifications (system tray popups) when Spotify changes songs")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SpotifyToaster")]
+[assembly: AssemblyProduct("spotifytoaster.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f44be778-a7e5-4b96-baa6-b1bae8ce80fe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
